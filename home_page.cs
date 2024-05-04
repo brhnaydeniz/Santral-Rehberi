3e4ec58b73ca91ffbb1fda808398d4bfcf688538
brhnaydeniz <burhan.aydeniz0101@hotmail.com>
2024-05-04 08:58:47 +0300

    Proje dosyası ekle.

---
+++ home_page.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Santral_Rehberi
+{
+    public partial class home_page : Form
+    {
+        public home_page()
+        {
+            InitializeComponent();
+            GradientBackground.Apply(this, Color.White, Color.Blue, LinearGradientMode.Vertical);
+        }
+
+        private void home_page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
